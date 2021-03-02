a7cf8d2d2a3521d12bf86170fdec46f8f1a94904
Jackson <jacksongodwin1517@gmail.com>
2021-03-01 18:46:59 -0600

    Search kinda works (if you click)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ChipComponent } from './chip/chip.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    ChipComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,13 +3,54 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
+  // styleUrls: ['./search.component.scss']
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
 
+  typing = false;
+  search = '';
+  tags = [
+    'vegitarian',
+    'pasta',
+    'slow cooker',
+    'soup',
+    'chicken',
+    'beef'
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showTags(): boolean {
+    if (this.search || this.typing==true) {
+      this.typing = true;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  debug() {
+    console.log(this.filterTags());
+  }
+
+  //return true if given tag fuzzily matches
+  filterTags() {
+    return this.tags.filter(x => this.doesTagMatch(x));
+  }
+
+  doesTagMatch(tag: string) {
+    // if (tag.includes(this.search[0])) { return true; } else { return false; }
+    let chars = this.search.split('');
+    for (let i = 0; i < chars.length; i++) {
+      if (!tag.includes(chars[i])) {
+        console.log(chars[i]);
+        return false;
+      }
+    }
+    return true;
   }
 
 }

56aeaf5a53aea49bfda7305d378bf4b4996fd909
Jackson <jackson@ganymede.home>
2021-05-15 16:52:59 -0500

    Added markers for motion

--- input.js
+++ input.js
@@ -1,42 +1,46 @@
-console.log('Hi from input');
-
-class keybind {
-    constructor(keyCode, func) {
-        this.keyCode = upcode;
-        this.func = func;
-        this.pressed = false;
+function inputInit() {
+    class keybind {
+        constructor(keyCode, func) {
+            this.keyCode = keyCode;
+            this.func = func;
+            this.pressed = false;
+        }
     }
+
+    //w: 87
+    //a: 65
+    //s: 83
+    //d: 68
+    KEYBINDS = [
+        /*'forward':*/ new keybind(87,()=>{
+
+        }),
+        /*'left':*/ new keybind(65,()=>{
+            player.rotation.z += 0.01;
+            player.rotation.z = Math.min(player.rotation.z, .5);
+        }),
+        /*'right':*/ new keybind(68,()=>{
+            player.rotation.z -= 0.01;
+            player.rotation.z = Math.max(player.rotation.z, -.5);
+        }),
+        /*'back':*/ new keybind(83,()=>{
+
+        }),
+    ]
+
+    const keyCodes = KEYBINDS.map(x => x.keyCode)
+    document.addEventListener('keydown', (e) => {
+        i = keyCodes.indexOf(e.keyCode);
+        if (i != -1) {
+            KEYBINDS[i]['pressed'] = true;
+            // KEYBINDS[i].down();
+        }
+    })
+    document.addEventListener('keyup', (e) => {
+        i = keyCodes.indexOf(e.keyCode);
+        if (i != -1) {
+            KEYBINDS[i]['pressed'] = false;
+            // KEYBINDS[i].up();
+        }
+    })
 }
-
-//w: 87
-//a: 65
-//s: 83
-//d: 68
-KEYBINDS = {
-    'forward': new keybind(87,()=>{
-
-    }),
-    'left': new keybind(65,()=>{
-        player.rotation.z = -2
-    }),
-    'right': new keybind(68,()=>{
-        player.rotation.z = 2
-    }),
-    'back': new keybind(83,()=>{
-
-    }),
-}
-
-const keyCodes = KEYBINDS.map(x => x.keyCode)
-document.addEventListener('keydown', (e) => {
-    i = keyCodes.indexOf(e.keyCode);
-    if (i != -1) {
-        KEYBINDS[i].pressed = true;
-    }
-})
-document.addEventListener('keyup', (e) => {
-    i = keyCodes.indexOf(e.keyCode);
-    if (i != -1) {
-        KEYBINDS[i].pressed = false;
-    }
-})
